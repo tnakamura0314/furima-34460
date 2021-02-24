2ec63f083accf554a86478a8df42de9fd53f0a6c
tnakamura0314 <reggae.a0215@gmail.com>
2021-02-24 09:38:55 +0900

    エラーメッセージの表示/パスワードのバリデーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,14 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname,presence: true
-  validates :last_name,presence: true
-  validates :first_name,presence: true
-  validates :last_name_kana,presence: true
-  validates :last_name_kana,presence: true
-  validates :birthday,presence: true
+  validates :nickname, presence: true
+  validates :last_name, presence: true
+  validates :first_name, presence: true
+  validates :last_name_kana, presence: true
+  validates :last_name_kana, presence: true
+  validates :birthday, presence: true
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'Include both letters and numbers' 
 
 end
