70b13602a8939620f47d94f53ca0c66f7681677d
tnakamura0314 <reggae.a0215@gmail.com>
2021-02-26 16:42:30 +0900

    ログインの有無による、出品ページへの遷移の条件分岐/active_hashの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,7 +4,13 @@ class ItemsController < ApplicationController
   end
 
   def new
+    if user_signed_in?
+      @item = Item.new
+    else
+      redirect_to new_user_session_path
+    end
   end
+
 
   private
 
