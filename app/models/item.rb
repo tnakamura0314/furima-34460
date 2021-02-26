024504b4f6bbbcc30f257e6307ac752970592461
tnakamura0314 <reggae.a0215@gmail.com>
2021-02-26 18:02:05 +0900

    Active_hashの設定/アソシエーションの記述/プルダウンリストのバリデーション変更

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,16 +3,26 @@ class Item < ApplicationRecord
   with_options presence: true do
     validates :name
     validates :text
+    validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: "Half-width number"}
+    validates :user
+  end
+
+  with_options dnumericality: { other_than: 1 } do
     validates :category_id
     validates :condition_id
     validates :shopping_charge_id
     validates :prefectures_id
     validates :day_to_ship_id
-    validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: "Half-width number"}
-    validates :user
   end
 
   belongs_to :user
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :shopping_charge
+  belongs_to :prefecture
+  belongs_to :day_to_ship
 
 end
