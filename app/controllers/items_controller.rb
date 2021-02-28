05dacd83cd41c55ae22bef1e072b39b4f3520019
tnakamura0314 <reggae.a0215@gmail.com>
2021-02-28 19:07:40 +0900

    商品の登録済みの値情報の表示/編集機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -23,6 +23,12 @@ class ItemsController < ApplicationController
   end
 
   def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    @item.update(item_params)
   end
 
   private
