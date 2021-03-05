ab9cbfc819102f052c4d229b6078b1762be44271
tnakamura0314 <reggae.a0215@gmail.com>
2021-03-05 20:15:40 +0900

    PAY.JPの実装/環境変数の設定

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -9,6 +9,7 @@ class OrdersController < ApplicationController
     @order_order_info = OrderOrderInfo.new(order_params)
     @item = Item.find(params[:item_id])
     if @order_order_info.valid?
+      pay_order
       @order_order_info.save
       redirect_to root_path
     else
@@ -19,6 +20,15 @@ class OrdersController < ApplicationController
   private
 
     def order_params
-      params.require(:order_order_info).permit(:postal_code, :prefectures_id, :municipality, :address, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+      params.require(:order_order_info).permit(:postal_code, :prefectures_id, :municipality, :address, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+    end
+
+    def pay_order
+      Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+      Payjp::Charge.create(
+        amount: @item.price,
+        card: order_params[:token],
+        currency: 'jpy'
+      )
     end
 end
--- app/models/order_order_info.rb
+++ app/models/order_order_info.rb
@@ -1,6 +1,6 @@
 class OrderOrderInfo
   include ActiveModel::Model
-  attr_accessor :postal_code, :prefectures_id, :municipality, :address, :building_name, :phone_number, :user_id, :item_id
+  attr_accessor :postal_code, :prefectures_id, :municipality, :address, :building_name, :phone_number, :user_id, :item_id, :token
 
   with_options presence: true do
     validates :postal_code, format: {with: /\A\d{3}[-]\d{4}\z/, message: "Input correctly"}
@@ -9,6 +9,7 @@ class OrderOrderInfo
     validates :phone_number, format: {with: /\A\d{10,11}\z/, message: "Input only number"}
     validates :user_id
     validates :item_id
+    validates :token
   end
 
   validates :prefectures_id, numericality: {other_than: 1, message: "Select"}
