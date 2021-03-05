a318da71ee0b5e60dfee1ad99ac8b45b1d21aa3b
tnakamura0314 <reggae.a0215@gmail.com>
2021-03-05 16:09:15 +0900

    テストコードの実装

--- spec/factories/order_order_infos.rb
+++ spec/factories/order_order_infos.rb
@@ -1,5 +1,11 @@
 FactoryBot.define do
   factory :order_order_info do
-    
+    postal_code {"123-4567"}
+    prefectures_id {Faker::Number.between(from: 2, to: 48)}
+    municipality {"横浜市緑区"}
+    address {"青山1-1-1"}
+    building_name {"柳ビル103"}
+    phone_number {"09071824624"}
+    item_id {1}
   end
 end
--- spec/models/order_order_info_spec.rb
+++ spec/models/order_order_info_spec.rb
@@ -1,5 +1,69 @@
 require 'rails_helper'
 
 RSpec.describe OrderOrderInfo, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '購入の保存' do
+    before do
+      user = FactoryBot.create(:user)
+      @order_order_info = FactoryBot.build(:order_order_info, user_id: user.id)
+    end
+
+    context '購入が保存できるとき' do
+      it 'すべての値が正しく入力されていれば保存できること' do
+        expect(@order_order_info).to be_valid
+      end
+    end
+
+    context '購入が保存できないとき' do
+      it 'postal_codeが空だと保存できない' do
+        @order_order_info.postal_code = ''
+        @order_order_info.valid?
+        expect(@order_order_info.errors.full_messages).to include("Postal code can't be blank")
+      end
+      it 'municipalityが空だと保存できない' do
+        @order_order_info.municipality = ''
+        @order_order_info.valid?
+        expect(@order_order_info.errors.full_messages).to include("Municipality can't be blank")
+      end
+      it 'addressが空だと保存できない' do
+        @order_order_info.address = ''
+        @order_order_info.valid?
+        expect(@order_order_info.errors.full_messages).to include("Address can't be blank")
+      end
+      it 'phone_numberが空だと保存できない' do
+        @order_order_info.phone_number = ''
+        @order_order_info.valid?
+        expect(@order_order_info.errors.full_messages).to include("Phone number can't be blank")
+      end
+      it 'prefectures_idが未選択だと保存できない' do
+        @order_order_info.prefectures_id = 1
+        @order_order_info.valid?
+        expect(@order_order_info.errors.full_messages).to include("Prefectures Select")
+      end
+      it 'postal_codeにハイフンがなければ保存できない（123-4567）' do
+        @order_order_info.postal_code = '1234567'
+        @order_order_info.valid?
+        expect(@order_order_info.errors.full_messages).to include("Postal code Input correctly")
+      end
+      it 'postal_codeは半角数字でなければ保存できない（123-4567）' do
+        @order_order_info.postal_code = '１２３-４５６７'
+        @order_order_info.valid?
+        expect(@order_order_info.errors.full_messages).to include("Postal code Input correctly")
+      end
+      it 'phone_numberは11桁以内でなければ保存できない（09012345678となる）' do
+        @order_order_info.phone_number = '090123456789'
+        @order_order_info.valid?
+        expect(@order_order_info.errors.full_messages).to include("Phone number Input only number")
+      end
+      it 'phone_numberは半角数字でなければ保存できない（09012345678となる）' do
+        @order_order_info.phone_number = '０９０１２３４５６７８'
+        @order_order_info.valid?
+        expect(@order_order_info.errors.full_messages).to include("Phone number Input only number")
+      end
+      it 'phone_numberはハイフンがあると保存できない（09012345678となる）' do
+        @order_order_info.phone_number = '090-123-567'
+        @order_order_info.valid?
+        expect(@order_order_info.errors.full_messages).to include("Phone number Input only number")
+      end
+    end
+  end
 end
